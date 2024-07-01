35320e740fce93a5da1462d024762799c1117f5f
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2024-07-01 13:50:33 +0400

    mod

---
+++ src/main/java/ru/furnygo/mixin/StringReaderMixin.java
@@ -0,0 +1,20 @@
+package ru.furnygo.mixin;
+
+import com.mojang.brigadier.StringReader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StringReader.class)
+public class StringReaderMixin {
+    // Author: lauron
+    @Inject(method = "isAllowedInUnquotedString", at = @At("RETURN"), remap = false, cancellable = true)
+    private static void onIsAllowedInUnquotedString(char c, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(
+                Character.isLetterOrDigit(c)
+                        || c == '_' || c == '-'
+                        || c == '.' || c == '+'
+        );
+    }
+}
